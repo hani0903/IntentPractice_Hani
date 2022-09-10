26e016e4fbcc5429d0aa72f5ca7a7bf32ec6e7f5
Hani <jhe93@naver.com>
2022-09-10 23:29:51 +0900

    메세지를 보내는 화면 / 받는 화면 추가 작성

---
+++ app/src/main/java/com/metacoding/intentpractice_hani/MessageActivity.kt
@@ -0,0 +1,11 @@
+package com.metacoding.intentpractice_hani
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
