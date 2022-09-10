d828d01af51670a9a87b4ccf25da2bfc611aff4d
Hani <jhe93@naver.com>
2022-09-10 20:56:11 +0900

    Initial commit

---
+++ app/src/main/java/com/metacoding/intentpractice_hani/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.metacoding.intentpractice_hani
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
